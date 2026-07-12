e92c6e31eb196daef793e8615e257848e7fe488e
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-07-12 15:18:23 +0200

    add `src/test/java/com/github/Test_50_enum.java`

--- src/test/java/com/github/Test_50_enum.java
+++ src/test/java/com/github/Test_50_enum.java
@@ -1,5 +1,6 @@
 package com.github;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -79,5 +80,55 @@ public class Test_50_enum {
         String expectedMessage = "Gotta keep working...";
         boolean areEqual = expectedMessage.equals(observedMessage);
         assertTrue(areEqual);
+    }
+
+    private enum Month {
+        JANUARY(1, "Jan"),
+        FEBRUARY(2, "Feb"),
+        MARCH(3, "Mar"),
+        APRIL(4, "Apr"),
+        MAY(5, "May"),
+        JUNE(6, "Jun"),
+        JULY(7, "Jul"),
+        AUGUST(8, "Aug"),
+        SEPTEMBER(9, "Sep"),
+        OCTOBER(10, "Oct"),
+        NOVEMBER(11, "Nov"),
+        DECEMBER(12, "Dec");
+
+        final int number;
+        final String abbreviatedName;
+
+        Month(int number, String abbreviatedName) {
+            this.number = number;
+            this.abbreviatedName = abbreviatedName;
+        }
+
+        public int getNumber() {
+            return number;
+        }
+
+        public String getAbbreviatedName() {
+            return abbreviatedName;
+        }
+    }
+
+    @Test
+    void callMethods() {
+        // Arrange.
+        Month february = Month.FEBRUARY;
+
+        // Act.
+        int observedNumber = february.getNumber();
+        String observedAbbreviatedName = february.getAbbreviatedName();
+
+        // Assert.
+        int expectedNumber = 2;
+        String expectedAbbreviatedName = "Feb";
+
+        assertEquals(expectedNumber, observedNumber);
+        assertTrue(
+            expectedAbbreviatedName.equals(observedAbbreviatedName)
+        );
     }
 }
